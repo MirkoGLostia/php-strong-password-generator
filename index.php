ac5256baf58568748ba210d91d9767eac1f27f94
MirkoGLostia <giggi.torre@mail.com>
2023-06-22 01:16:03 +0200

    aggiunta dello stile

--- index.php
+++ index.php
@@ -13,32 +13,116 @@
 
     $lenghtPsw = (int)$_GET["numberCharacters"];
 
-    echo var_dump($lenghtPsw);
+    // echo var_dump($lenghtPsw);
 
     include 'functions/functions.php';
 
     $password = RandomPassword($lenghtPsw);
 
-    echo var_dump($password);
+    // echo var_dump($password);
 
     ?>
+
+    <style>
+        header {
+            width : 100%;
+            text-align : center;
+        }
+
+        h1 {
+            color : rgb(121, 139, 153);
+        }
+
+        h3 {
+            color : white;
+        }
+
+        body {
+            background-color : rgb(0, 22, 50);
+        }
+        
+        .form-banner {
+            background-color : rgb(207, 244, 252);
+            color : rgb(76, 137, 147);
+            width : 60%;
+            margin : 0 auto;
+            padding : 20px;
+            border-radius : 10px;
+        }
+
+        #form-container {
+            background-color : white;
+            width : 60%;
+            margin : 0 auto;
+            border-radius : 10px;
+            text-align : center;
+        }
+
+        form > div {
+            margin : 10px;
+            padding : 30px;
+        }
+        
+        .form-flex {
+            display : flex;
+            flex-wrap: wrap;
+        }
+
+        div > .left-side, .right-side {
+            width : 50%;
+        }
+
+        #password-container {
+            width : 40%;
+            margin : 0 auto;
+            padding : 15px;
+            border-radius : 10px;
+            background-color : white;
+            text-align : center;
+            font-size : 20px;
+        }
+    </style>
 
 </head>
 <body>
 
+    <header>
+        <h1>
+            Strong Password Generator
+        </h1>
+
+        <h3>
+            Genera una password sicura
+        </h3>
+    </header>
+
     <main>
-        <form method="get">
-            <label for="numberCharacters">lenght of password: </label>
-            <input type="number" name="numberCharacters" id="numberCharacters" size="5" min="1" max="50" value="1"> <!-- il value per forzare un valore iniziale -->
-            <button type="submit">GENERATE</button>
-        </form>
+        <?php
+            echo '<div class="form-banner">
+            <span> Nessun parametro valido inserito </span>
+            </div>'
+        ?>
+        <section id="form-container"> 
+            <form method="get">
+                <div class="form-flex">
+                    <div class="left-side">
+                        <label for="numberCharacters">lenght of password: </label>
+                    </div>
+        
+                    <div class="right-side">
+                        <input type="number" name="numberCharacters" id="numberCharacters" size="5" min="1" max="50"> <!-- il value per forzare un valore iniziale -->
+                    </div>
+                </div>
+                <div>
+                    <button type="submit">GENERATE</button>
+                </div>
+            </form>
+        </section>
 
         <section>
-            <div>
+            <div id="password-container">
                 <span>
-                    <?php
-                        echo $password
-                    ?>
+                    Your password is: <?php echo $password ?>
                 </span>
             </div>
         </section>
